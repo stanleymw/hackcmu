264b28226216a8158634929b2879758b53f0c7d3
stanleymw <107821509+stanleymw@users.noreply.github.com>
2025-09-13 17:46:31 -0400

    Win popup goes in the middle of the screen

--- src/ui/win_screen.rs
+++ src/ui/win_screen.rs
@@ -1,12 +1,17 @@
-use bevy::ecs::{
-    error::Result,
-    event::EventWriter,
-    resource::Resource,
-    system::{Local, Query, Res, ResMut},
+use bevy::{
+    ecs::{
+        error::Result,
+        event::EventWriter,
+        resource::Resource,
+        system::{Local, Query, Res, ResMut},
+    },
+    math::Vec2,
+    window::{MonitorSelection, WindowPosition},
 };
 use bevy_egui::{
     EguiContexts,
-    egui::{self, RichText},
+    egui::{self, Align2, Pos2, RichText, ViewportInfo, emath},
+    helpers::vec2_into_egui_vec2,
 };
 use egui_commonmark::{CommonMarkCache, CommonMarkViewer};
 
@@ -22,13 +27,15 @@ pub fn win_screen(
     mut events: EventWriter<GameResetEvent>,
 ) -> Result {
     if has_won.0 {
-        egui::Window::new("You Win!").show(contexts.ctx_mut()?, |ui| {
-            if ui.button("Next Level").clicked() {
-                current_lvl.index = current_lvl.index.wrapping_add(1);
-                events.write(GameResetEvent);
-                has_won.0 = false;
-            }
-        });
+        egui::Window::new("You Win!")
+            .anchor(Align2::CENTER_CENTER, emath::vec2(0.0, 0.0))
+            .show(contexts.ctx_mut()?, |ui| {
+                if ui.button("Next Level").clicked() {
+                    current_lvl.index = current_lvl.index.wrapping_add(1);
+                    events.write(GameResetEvent);
+                    has_won.0 = false;
+                }
+            });
     }
 
     Ok(())
