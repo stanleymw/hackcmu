c41dd13daeffe3f3b2a6daefba49971f59a964f4
Eoghanmc22 <31516877+Eoghanmc22@users.noreply.github.com>
2025-09-14 03:58:34 +0000

    Build: (b9d6b87) Fix wasm build hopefully

---
+++ hackcmu_bg.wasm.d.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const closure2081_externref_shim: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h0412aa44b2792f49: (a: number, b: number) => void;
+export const closure8917_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__hc46375889d6b9e37: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
